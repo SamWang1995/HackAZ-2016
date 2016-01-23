4cf74ece28600e09269064b9194f0a5f1e34276e
mclehman <maxfieldlehman@gmail.com>
2016-01-23 13:02:21 -0700

    Added search button and skeleton search view controller

---
+++ HackAZ/HackAZ/SearchVC.swift
@@ -0,0 +1,35 @@
+//
+//  SearchVC.swift
+//  HackAZ
+//
+//  Created by Maxfield Lehman on 1/23/16.
+//  Copyright © 2016 Samuel Wang. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
