1a92df2181ed2d1aee9bfc82360771fd272f44a5
mclehman <maxfieldlehman@gmail.com>
2016-01-23 00:48:33 -0700

    Added skeleton for sign up page

---
+++ HackAZ/HackAZ/SignUpVC.swift
@@ -0,0 +1,44 @@
+//
+//  SignUpVC.swift
+//  HackAZ
+//
+//  Created by Maxfield Lehman on 1/23/16.
+//  Copyright © 2016 Samuel Wang. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func confirmSignUp(sender: AnyObject) {
+        // Verify valid email
+        
+        // Verify valid school
+        
+        // Hash/store user/pass
+        
+        // Segue to main view
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
