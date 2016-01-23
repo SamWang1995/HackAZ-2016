4582464b34ef1364512dbc416761b3fa38e49891
SamWang1995 <Sam@Samuels-MacBook-Air.local>
2016-01-23 05:02:37 -0700

    Added cell prototype

---
+++ HackAZ/HackAZ/MainCell.swift
@@ -0,0 +1,26 @@
+//
+//  MainCell.swift
+//  HackAZ
+//
+//  Created by Samuel Wang on 1/23/16.
+//  Copyright © 2016 Samuel Wang. All rights reserved.
+//
+
+import UIKit
+
+class MainCell: UITableViewCell {
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
