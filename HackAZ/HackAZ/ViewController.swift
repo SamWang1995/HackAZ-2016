c280bad60b6f08dc46ab78f8de75b49b07b10879
SamWang1995 <Sam@Samuels-MacBook-Air.local>
2016-01-22 23:25:32 -0700

    SignUpPage

--- HackAZ/HackAZ/ViewController.swift
+++ HackAZ/HackAZ/ViewController.swift
@@ -14,7 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func LoginButton(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func SignUpButton(sender: AnyObject) {
+        performSegueWithIdentifier("toSignUp", sender: self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
